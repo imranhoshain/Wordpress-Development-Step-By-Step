f0031d63d058056f1c0277e2d4f731897e662266
princ-imran <princ.imran@gmail.com>
2018-07-07 12:27:41 +0600

    Page.php file develop

--- page.php
+++ page.php
@@ -47,46 +47,48 @@ get_header();
 
 
 <?php if($enable_title == true) : ?> 
-<div class="page-banner">
-			<img src="<?php the_post_thumbnail_url('large') ?>" alt="Page Banner">
+
+<div class="rideo-page-title">
+	<div class="page-banner">
+		<img src="<?php the_post_thumbnail_url('large') ?>" alt="Page Banner">
+		<!-- Use breadcamp for this function
 		<div class="container">
-		<div class="row ">
-			<div class="col-md-12 text-<?php echo $text_title_direction; ?>">
-				<?php if(empty($custom_title)) : ?>
-				<h2><?php the_title(); ?></h2>
-			<?php else : ?>
-				<div class="restaurant-page-custom-title">
-					<?php echo wpautop($custom_title); ?>
+			<div class="row ">
+				<div class="col-md-12 text-<?php echo $text_title_direction; ?>">
+					<?php if(empty($custom_title)) : ?>
+					<h2><?php the_title(); ?></h2>
+					<?php else : ?>
+					<div class="rideo-page-custom-title">
+						<?php echo wpautop($custom_title); ?>
+					</div>
+					<?php endif; ?>
+					<?php if(function_exists('bcn_display')){bcn_display(); } //Install Breadcamp Next plugin?>
 				</div>
-			<?php endif; ?>
-				<?php if(function_exists('bcn_display')){bcn_display(); } ?>
 			</div>
-		</div>
+		</div> 
+	-->
 	</div>
 </div>
-
 <?php endif; ?>
 
+<?php while ( have_posts() ) : the_post(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
+			<div class="container">
+				<div class="row">					
+					<?php						
+						get_template_part( 'template-parts/content', 'page' );
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', 'page' );
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) :
+								comments_template();
+							endif;							
+					?>					
+				</div>
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
+<?php endwhile; // End of the loop.?>
 
 <?php
-get_sidebar();
 get_footer();
